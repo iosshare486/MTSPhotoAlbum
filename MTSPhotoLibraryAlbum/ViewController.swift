b82c458b268dbdb78805f90266acc0047efdebfb
zhangjunjiecq <zhangjunjie@caiqr.com>
2018-07-17 12:06:27 +0800

    配置Pod 进行测试

--- MTSPhotoLibraryAlbum/ViewController.swift
+++ MTSPhotoLibraryAlbum/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        MTSPhotoAlbum.default.electedImgs = { (imgs) in
+            
+        }
+        
+        
+        MTSPhotoAlbum.callPhotoAlbum(launchViewController: self, imageLimit: 2)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
